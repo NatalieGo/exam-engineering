10cfc59667439d8a245955eb898cc71a7dc78be8
NatalieGo <nataligorod@yandex.ru>
2025-06-28 21:18:52 +0300

    WIP: локальные изменения перед pull

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -4,12 +4,14 @@ from airflow.utils.dates import days_ago
 import subprocess
 import os
 
-# Укажем пути к скриптам и логам
+# Пути
 dags_folder = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.abspath(os.path.join(dags_folder, '..', 'etl'))
 LOG_DIR = os.path.abspath(os.path.join(dags_folder, '..', 'logs'))
+RESULTS_DIR = os.path.abspath(os.path.join(dags_folder, '..', 'results'))
 
 os.makedirs(LOG_DIR, exist_ok=True)
+os.makedirs(RESULTS_DIR, exist_ok=True)
 
 default_args = {
     'owner': 'NatalieGo',
@@ -22,22 +24,22 @@ dag = DAG(
     'breast_cancer_pipeline',
     default_args=default_args,
     description='ETL pipeline for Breast Cancer dataset',
-    schedule_interval=None,  # отключено расписание
+    schedule_interval=None,
 )
 
-def run_script(script_name):
+def run_script(script_name, args=None):
     """
-    Запускает указанный скрипт и сохраняет лог
+    Запускает скрипт с аргументами и сохраняет лог
     """
     script_path = os.path.join(BASE_DIR, script_name)
     log_path = os.path.join(LOG_DIR, f"{script_name}.log")
 
+    command = ['python3', script_path]
+    if args:
+        command += args
+
     with open(log_path, "w") as log_file:
-        process = subprocess.Popen(
-            ['python3', script_path],
-            stdout=log_file,
-            stderr=log_file
-        )
+        process = subprocess.Popen(command, stdout=log_file, stderr=log_file)
         process.communicate()
 
     if process.returncode != 0:
@@ -58,13 +60,26 @@ clean_data_task = PythonOperator(
 
 train_model_task = PythonOperator(
     task_id='train_model',
-    python_callable=lambda: run_script('train_model.py'),
+    python_callable=lambda: run_script(
+        'train_model.py',
+        args=[
+            '--input_path', 'breast_cancer_clean.csv',
+            '--model_path', 'results/model.pkl'
+        ]
+    ),
     dag=dag,
 )
 
 calc_metrics_task = PythonOperator(
     task_id='calc_metrics',
-    python_callable=lambda: run_script('calc_metrics.py'),
+    python_callable=lambda: run_script(
+        'calc_metrics.py',
+        args=[
+            '--input_path', 'breast_cancer_clean.csv',
+            '--model_path', 'results/model.pkl',
+            '--output_path', 'results/metrics.json'
+        ]
+    ),
     dag=dag,
 )
 
